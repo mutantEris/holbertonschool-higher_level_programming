0079c9b6f24209b405a3618810113384a63aa374
root <mmires@live.com>
2021-09-30 22:42:59 +0000

    MAXIMUM POWER

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     biggest = my_list[0]
     for x in my_list:
-        if my_list[x] > biggest:
-            biggest = my_list[x+1]
+        if x > biggest:
+            biggest = x
     return biggest
