3669ee14877080215780dae5a5a567410960ec56
root <mmires@live.com>
2021-09-30 19:20:41 +0000

    does what i want

---
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list.copy()
+    elif idx >= len(my_list):
+        return my_list.copy()
+    else:
+        my_copy = my_list.copy()
+        my_copy[idx] = element
+        return my_copy
