59b28e877ca9f66010926c6f1f8a997e0ea98f7f
root <mmires@live.com>
2021-09-30 20:10:07 +0000

    matrix

---
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in range(len(matrix)):
+        for y in matrix[x]:
+            print("{}".format(y), end=" ")
+        print()
