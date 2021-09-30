5458d3502eacf965bf87452153e5de8c539e97a0
root <mmires@live.com>
2021-09-30 04:59:06 +0000

    5

--- 0x01-python-import_modules/5-raise_exception_msg.py
+++ 0x01-python-import_modules/5-raise_exception_msg.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def raise_exception_msg(message=""):
-        raise NameError(message)
+def raise_exception_msg(message=""):
+    raise NameError(message)
