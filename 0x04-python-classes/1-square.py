271446d133696473aa1e9134d1c468d0b6bbc9d5
root <mmires@live.com>
2021-10-06 18:31:42 +0000

    e

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
         """defines self"""
 
         self.__size = size
-        if type size is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
