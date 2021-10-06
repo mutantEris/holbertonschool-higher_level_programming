de30f23eb77aa4cb0ee5b383259a4aae5694f8c5
root <mmires@live.com>
2021-10-06 18:40:02 +0000

    calculates squarea area

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module used to create class square"""
+
+
+class Square:
+    """defines object by size"""
+    def __init__(self, size=0):
+        """defines self"""
+
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
