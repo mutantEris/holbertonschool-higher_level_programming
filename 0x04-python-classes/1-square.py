33646f42fc00fc725a9ba43a015bec824ee2db18
root <mmires@live.com>
2021-10-06 18:28:21 +0000

    new comment

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -5,6 +5,8 @@
 class Square:
     """defines object by size"""
     def __init__(self, size=0):
+        """defines self"""
+
         self.__size = size
         if type size is not int:
             raise TypeError("size must be an integer")
