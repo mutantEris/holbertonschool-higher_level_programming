a62684706ab6a7a7d4afad01c13ad252bfb2c9e9
root <mmires@live.com>
2021-10-13 01:14:53 +0000

    changed width to height

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
         tallnt
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
