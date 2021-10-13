87d5d7a28ddd2757655cabd48275cb574d4111f5
root <mmires@live.com>
2021-10-13 01:39:14 +0000

    fixed probably

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -61,5 +61,5 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return 0
-        else:
-            return 2(self.width + self.height)
+            perimeter = 2*(self.width + self.height)
+            return perimeter
