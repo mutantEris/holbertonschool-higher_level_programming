88e7e0f99edb801c4d5ad93c3081058b7fc16f42
root <mmires@live.com>
2021-10-13 02:29:29 +0000

    rectangle the second

---
+++ 0x06-python-more_classes/3-rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""
+behold my rectangle
+"""
+
+
+class Rectangle:
+    """
+    I am going to create a rectange class and define it
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        wow it sure is wide here
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        tall
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        long
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        tallnt
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        how big it be
+        """
+        area = self.height * self.width
+        return area
+
+    def perimeter(self):
+        """
+        how big its shell be
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        perimeter = 2 * (self.width + self.height)
+        return perimeter
+
+    def __str__(self):
+        """
+        stringly bingly
+        """
+        if self.width == 0 or self.height == 0:
+            return""
+        stringly = ""
+        for y in range(self.height - 1):
+            stringly += "#" * self.width + "\n"
+            if y is self.height - 2:
+                stringly += "#" * self.width
+        return stringly
+    def __repr__(self):
+        """
+        shows bigfulness of rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return ("Rectangle({}, {})".format(self.width, self.height))
