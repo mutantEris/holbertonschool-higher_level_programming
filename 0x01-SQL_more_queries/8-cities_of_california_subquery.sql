e8b7e113677ff6dd8f1c8628902fef9784b03eae
mutantEris <mmires@live.com>
2021-11-03 18:13:25 -0700

    work

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Lists California
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.states_id = states.id
+WHERE cities.state_id = states.id
 AND states.name = 'California'
 ORDER BY cities.id ASC;
