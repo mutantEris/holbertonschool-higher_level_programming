9ccf0538675eaa0e9ec201165c07def9a9d7c642
mutantEris <mmires@live.com>
2021-11-03 18:33:28 -0700

    typo

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
 -- LISTS SHOWS
-SELECT a.title, b.genre_id FROM tv_shows a, tv_shows_genres b WHERE b.show_id = a.id
+SELECT a.title, b.genre_id
+FROM tv_shows a, tv_show_genres b
+WHERE b.show_id = a.id
 ORDER BY a.title ASC, b.genre_id ASC;
