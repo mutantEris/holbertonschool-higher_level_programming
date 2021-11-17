15592a69de6547744723bec228016ab6e8f677c4
mutantEris <mmires@live.com>
2021-11-17 13:58:26 -0800

    moo

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""behold"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db = sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
