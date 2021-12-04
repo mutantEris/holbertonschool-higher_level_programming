9b3225ff928c0bf245ed3818649c0c715bae709d
mutantEris <mmires@live.com>
2021-12-04 13:23:31 -0800

    pushing before sandbox deletion

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Sends Email Address"""
+
+import urllib
+from urllib import request, parse
+from sys import argv
+
+
+
+if __name__ == "__main__":
+
+    values = {'email' : argv[2] }
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print(html)
