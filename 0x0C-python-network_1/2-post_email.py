ab180499db213ee1da438af31d5a4814a0b831a0
mutantEris <mmires@live.com>
2021-12-04 21:41:23 +0000

    pepper likes it

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -5,11 +5,9 @@ import urllib
 from urllib import request, parse
 from sys import argv
 
-
-
 if __name__ == "__main__":
 
-    values = {'email' : argv[2] }
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
