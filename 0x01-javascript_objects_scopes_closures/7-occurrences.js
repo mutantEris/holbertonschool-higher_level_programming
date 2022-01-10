063277128ae8ed0bdb20988d059df68dbb7ce144
mutantEris <mmires@live.com>
2022-01-10 14:01:39 -0600

    space removal in note

--- 0x01-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x01-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const instance of list) {
-    if (instance === searchElement) { count++; } //search element being the requested occurance being counted
+    if (instance === searchElement) { count++; } // search element being the requested occurance being counted
   }
   return count;
 };
