99a70d8dadaab80d01b8bb74a2854f5c27d845ad
mutantEris <mmires@live.com>
2022-01-11 11:23:30 -0800

    adding files

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let fs require ('fs')
+const fs = require('fs');
 
 fs.readfile(process.argv[2], 'utf-8', (err, contents) => {
-    if (!err) {
-        console.log(contents.toString());
-    } else {
-        console.log(err)
-    }
-})
+  if (!err) {
+    console.log(contents.toString());
+  } else {
+    console.log(err);
+  }
+});
