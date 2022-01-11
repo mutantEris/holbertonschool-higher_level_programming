5120d2335b5a0b2030918ed42dc7da683359d4b4
mutantEris <mmires@live.com>
2022-01-11 11:28:39 -0800

    fixed typo errors

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readfile(process.argv[2], 'utf-8', (err, contents) => {
+fs.readFile(process.argv[2], 'utf-8', (err, contents) => {
   if (!err) {
     console.log(contents.toString());
   } else {
