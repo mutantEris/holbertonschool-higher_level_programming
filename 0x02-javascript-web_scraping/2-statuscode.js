b86b2ceb37ef238349007e40239e1f7dd41ff2fb
mutantEris <mmires@live.com>
2022-01-11 19:32:07 -0800

    works

---
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  console.log('code: ' + (response && response.statusCode));
+  if (error) {
+    console.log(error);
+  }
+});
