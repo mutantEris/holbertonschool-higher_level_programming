70534f5d6d688d0b6b8ef133d4859e843e505aa4
mutantEris <mmires@live.com>
2022-01-15 13:36:34 -0600

    Made 6

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const list = JSON.parse(body);
+    const toDidList = {};
+
+    for (let x = 0; x < list.length; x++) {
+      if (list[x].completed) {
+        if (toDidList[list[x].user_id]) {
+          toDidList[list[x].user_id]++;
+        } else {
+          toDidList[list[x].user_id] = 1;
+        }
+      }
+    }
+    console.log(toDidList);
+  }
+});
