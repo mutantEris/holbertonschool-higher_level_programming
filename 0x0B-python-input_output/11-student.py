565cae76ffcb6dadfcf099de20641177a9e4553f
mutantEris <mmires@live.com>
2022-01-21 13:44:38 -0800

    pycodestyle

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,7 +9,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         """retrieves dictionary"""
         if attrs is None or type(attrs) is not list:
