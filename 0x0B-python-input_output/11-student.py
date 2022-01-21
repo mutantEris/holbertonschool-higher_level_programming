fcb023755000d52f151b06eadf1e07cb1e27ec20
mutantEris <mmires@live.com>
2022-01-21 13:42:33 -0800

    student help

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Creates a class"""
+
+
+class Student:
+    """the student class"""
+    def __init__(self, first_name, last_name, age):
+        """initializes attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self, attrs=None):
+        """retrieves dictionary"""
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for x in self.__dict__:
+                if x in attrs:
+                    my_dict[x] = self.__dict__[x]
+            return my_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of student"""
+        for x in json:
+            self.__dict__[x] = json[x]
