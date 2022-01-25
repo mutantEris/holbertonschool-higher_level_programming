27c8b04ccf7c088c7ed6cb79ac3ad01661e899a6
mutantEris <mmires@live.com>
2022-01-25 16:13:53 -0600

    2 & 3 tasks

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""rectangle class"""
+
+
+from re import X
+from turtle import window_height
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle importation"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """The wide"""
+        return self.__width
+
+    @property
+    def height(self):
+        """The tall"""
+        return self.__height
+
+    @property
+    def x(self):
+        """The x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """The y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """the wides"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """the talls"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """the xs"""
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """the wise"""
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
