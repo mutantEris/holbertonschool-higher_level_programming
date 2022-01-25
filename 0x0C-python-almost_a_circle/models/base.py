534e9d25d82597e6738889891c3ed326e0f8541a
mutantEris <mmires@live.com>
2022-01-25 15:37:01 -0600

    base but superior

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Base class for base class"""
+
 
 class Base:
+    """the class that is base"""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id is None: 
+        """class constructor"""
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else: self.id = id
+        else:
+            self.id = id
