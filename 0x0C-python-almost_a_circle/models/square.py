f99962c131c53ad9a5c53f5da15de0d908513e72
mutantEris <mmires@live.com>
2022-01-26 12:10:34 -0600

    sqauer

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,3 +7,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
     def __str__(self):
         """super class"""
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.size)
