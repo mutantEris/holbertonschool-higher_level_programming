8fbfa5f9afad8dafaddafee92ebfda0c7d8d185c
mutantEris <mmires@live.com>
2022-01-26 12:34:25 -0600

    made width public

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """The bigs"""
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
