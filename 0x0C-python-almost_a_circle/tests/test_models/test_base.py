96c690714106c567a5d76ec431b816f0b83e7b74
mutantEris <mmires@live.com>
2022-01-27 12:48:20 -0600

    testing

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""test file for base"""
+
+
+import unittest
+from models.base import Base
+
+
+class BaseTest(unittest.TestCase):
+    """basetest test testing"""
+
+    def test_id(self):
+        """stringle"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base(None)
+        self.assertEqual(b2.id, 2)
+
+    def test_ids(self):
+        """stringles"""
+        b1 = Base(r)
+        self.assertEqual()
+
+
+    if __name__ == '__main__':
+        unittest.main()
