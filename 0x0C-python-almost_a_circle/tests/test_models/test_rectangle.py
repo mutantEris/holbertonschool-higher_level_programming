302fccceb0af17eb5ac77ff58a9b870bfa002b2a
mutantEris <mmires@live.com>
2022-01-28 17:04:28 -0600

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,7 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class RectangleTest(unittest.TestCase):
     """rectangletest test testing"""
 
@@ -28,12 +29,13 @@ class RectangleTest(unittest.TestCase):
             r6 = Rectangle(1, 2, "3", 4)
         with self.assertRaises(TypeError):
             r7 = Rectangle(1, 2, 3, "4")
-    
+
     def test_toomany(self):
         """too many cooks"""
         r1 = Rectangle(1, 2, 3, 4, 5)
         r8 = Rectangle(1, 1)
         r9 = Rectangle(1, 1, 0)
+        r10 = r1.to_dictionary()
         self.assertEqual(r1.id, 5)
         with self.assertRaises(ValueError):
             r2 = Rectangle(-1, 2)
@@ -51,6 +53,27 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(r1.__str__(), "[Rectangle] (5) 3/4 - 1/2")
         self.assertEqual(r8.display(), None)
         self.assertEqual(r9.display(), None)
-        
+        self.assertIsInstance(r10, dict)
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+        r1.update(89, 1)
+        self.assertEqual(r1.width, 1)
+        r1.update(89, 1, 2)
+        self.assertEqual(r1.height, 2)
+        r1.update(89, 1, 2, 3)
+        self.assertEqual(r1.x, 3)
+        r1.update(89, 1, 2, 3, 4)
+        self.assertEqual(r1.y, 4)
+        r1.update(**{'id': 89})
+        self.assertEqual(r1.id, 89)
+        r1.update(**{'id': 89, 'width': 1})
+        self.assertEqual(r1.width, 1)
+        r1.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(r1.height, 2)
+        r1.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(r1.x, 3)
+        r1.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(r1.y, 4)
+
     if __name__ == '__main__':
         unittest.main()
